1ed03b89b69cdf64b32a6821bcf7e5d6e290ee75
Michelle Marin <michelle@carinocreative.com>
2014-11-24 14:31:07 -0500

    changes so far

--- recipe-app/js/app.js
+++ recipe-app/js/app.js
@@ -50,3 +50,15 @@ $("#myForm").validate( {
   }
 
 });
+
+/* App.GravatarImageComponent = Ember.Component.extend({
+  size: 200,
+  email: '',
+
+  gravatarUrl: function() {
+    var email = this.get('email'),
+        size = this.get('size');
+
+    return 'http://www.gravatar.com/avatar/' + md5(email) + '?s=' + size;
+  }.property('email', 'size')
+}); */
